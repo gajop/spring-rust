12371457bd54528d3db00a8b9a4e9514a93e9cdb
Gajo Petrovic <gajopetrovic@gmail.com>
2026-08-20 07:42:50 +0900

    expand core wasm callin bindings

--- rts/WasmInterface/WasmCoreBindings.h
+++ rts/WasmInterface/WasmCoreBindings.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include <cstdint>
 #include <string>
 
 #include "NativeInterface/NativeInterface.h"
@@ -41,16 +42,43 @@ public:
 
 	bool Bind(wasmtime_context_t* context, const wasmtime_instance_t& instance,
 		std::string& error);
-	bool GameFrame(wasmtime_context_t* context, std::int32_t frame, std::string& error) const
-	{
-		return gameFrame.Call(context, frame, error);
-	}
+
+	bool GameFrame(wasmtime_context_t* context, std::int32_t frame, std::string& error) const;
+	bool GameFramePost(wasmtime_context_t* context, std::int32_t frame, std::string& error) const;
+	bool Update(wasmtime_context_t* context, float deltaSeconds, std::string& error) const;
+	bool UnitCreated(wasmtime_context_t* context, std::int32_t unitID,
+		std::int32_t unitDefID, std::int32_t unitTeam, std::int32_t builderID,
+		std::string& error) const;
+	bool UnitPreDamaged(wasmtime_context_t* context, std::int32_t unitID,
+		std::int32_t unitDefID, std::int32_t unitTeam, float damage, bool paralyzer,
+		std::int32_t weaponDefID, std::int32_t projectileID, std::int32_t attackerID,
+		std::int32_t attackerDefID, std::int32_t attackerTeam,
+		float& newDamage, float& impulseMult, std::string& error) const;
+	bool AllowUnitCreation(wasmtime_context_t* context, std::int32_t unitDefID,
+		std::int32_t builderID, std::int32_t builderTeam, bool hasBuildInfo,
+		float buildX, float buildY, float buildZ, std::int32_t buildFacing,
+		bool& allow, bool& dropOrder, std::string& error) const;
+	bool DrawWorld(wasmtime_context_t* context, std::string& error) const;
+
 	bool HasGameFrame() const { return gameFrame.Present(); }
+	bool HasGameFramePost() const { return gameFramePost.Present(); }
+	bool HasUpdate() const { return update.Present(); }
+	bool HasUnitCreated() const { return unitCreated.Present(); }
+	bool HasUnitPreDamaged() const { return unitPreDamaged.Present(); }
+	bool HasAllowUnitCreation() const { return allowUnitCreation.Present(); }
+	bool HasDrawWorld() const { return drawWorld.Present(); }
+
 	HostState& Host() { return host; }
 
 private:
 	HostState host;
 	I32ToVoidExport gameFrame;
+	I32ToVoidExport gameFramePost;
+	RawExport update;
+	RawExport unitCreated;
+	RawExport unitPreDamaged;
+	RawExport allowUnitCreation;
+	RawExport drawWorld;
 };
 
 #endif
