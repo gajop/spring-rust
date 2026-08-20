3e40a09e25d9ae5c9f8b340739459e9527234b8c
Gajo Petrovic <gajopetrovic@gmail.com>
2026-08-20 19:39:24 +0900

    wasm: add borrowed UnitsQuery Core binding interface

---
+++ rts/WasmInterface/WasmCoreUnitsQueryBorrowedBindings.h
@@ -0,0 +1,16 @@
+/* This file is part of the Spring engine (GPL v2 or later), see LICENSE.html */
+
+#pragma once
+
+#include <string>
+
+#include "WasmCoreBindings.h"
+
+namespace recoil::wasm::core {
+
+#if defined(RECOIL_WASMTIME_AVAILABLE)
+bool RegisterUnitsQueryBorrowedImports(wasmtime_linker_t* linker, HostState* state,
+	std::string& error);
+#endif
+
+} // namespace recoil::wasm::core
