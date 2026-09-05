9af4f65bbeabfc573d1c1d632429f9b93be4d61a
Gajo Petrovic <gajopetrovic@gmail.com>
2026-09-05 23:25:02 +0900

    spring-addons: expose addon context and resources

--- rust/crates/spring-addons/src/lib.rs
+++ rust/crates/spring-addons/src/lib.rs
@@ -1,10 +1,12 @@
 #![no_std]
+#![doc = include_str!("../REENTRANCY.md")]
 
 extern crate alloc;
 
 pub mod event;
 pub mod macros;
 pub mod rules;
+pub mod runtime;
 pub mod ui;
 pub mod unsynced;
 
@@ -12,9 +14,8 @@ pub mod reexports {
     pub use spring::*;
 }
 
-pub use event::{CommandEvent, EventResult, KeyEvent, ViewGeometry};
-pub use rules::{
-    Gadget, GadgetHandler, PendingRulesEvent, UnitDestroyedEvent, UnitPreDamagedEvent,
-};
+pub use event::{CommandEvent, EventResult, KeyEvent, PendingRulesEvent, ViewGeometry};
+pub use rules::{Gadget, GadgetHandler, UnitDestroyedEvent, UnitPreDamagedEvent};
+pub use runtime::{AddonContext, Resources};
 pub use ui::{Widget, WidgetHandler};
 pub use unsynced::{UnsyncedAddon, UnsyncedHandler};
